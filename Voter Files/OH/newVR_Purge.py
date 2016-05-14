57cbdb968d86c49f5d546139e6a0565d05b04e2e
tylerjrichards <tylerjrichards@gmail.com>
2016-05-14 08:22:12 -0400

    Update newVR_Purge.py

--- Voter Files/OH/newVR_Purge.py
+++ Voter Files/OH/newVR_Purge.py
@@ -12,15 +12,18 @@ import matplotlib.pyplot as plt
 import sys #only needed to determine Python version number
 import matplotlib #only needed to determine Matplotlib version number and to use for data analysis, super fun
 
-#creates two dataframes from csv files 
+#creates two dataframes from csv files  
 Location = r'C:\Users\Tyler\Documents\Data Projects\National Voter Data File\Ohio\VINTON.csv'
 df = pd.read_csv(Location)
-df['Type'] = ("PURGE")
 Location2 = r'C:\Users\Tyler\Documents\Data Projects\National Voter Data File\Ohio\VINTON2.csv'
 df2 = pd.read_csv(Location2)
+#lables the two dataframes 
+df['Type'] = ("PURGE")
 df2['Type'] = ("NEWVR")
+#concatenates the two dataframes and drops the duplicates
 frames = [df, df2]
 df4 = pd.concat(frames)
 df5=df4.drop_duplicates(['SOS_VOTERID'], keep=False)
+#the remaining file contains the information of Purged and new individuals labeled as such in the column 'Type'
 df5.to_csv(r'C:\Users\Tyler\Documents\Data Projects\National Voter Data File\Ohio\change.csv')
 

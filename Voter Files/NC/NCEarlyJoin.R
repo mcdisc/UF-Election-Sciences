b5853be1b73ceb4e27446123330bdffa0af31853
Pedro Otalora <potalora@gmail.com>
2016-12-07 15:50:14 -0500

    Use fread instead of read.table

--- Voter Files/NC/NCEarlyJoin.R
+++ Voter Files/NC/NCEarlyJoin.R
@@ -1,9 +1,10 @@
 library(dplyr)
 library(tidyr)
 library(magrittr)
+library(data.table)
 setwd('/Users/Potalora/Desktop')
 
-NC2016 <- read.delim('/Users/Potalora/Downloads/ncvoter_Statewide.txt', header = TRUE, sep = '\t')
+NC2016 <- fread('/Users/Potalora/Downloads/ncvoter_Statewide.txt')
 Early2016 <- read.csv('/Users/Potalora/Downloads/absentee11xx08xx2016-10.csv')
 Early2016less <- subset(Early2016, select = -c(14:21, 25:26, 34))
 NC2016less <- subset(NC2016, select = -c(1:8, 14:26, 28:68, 70:71))
